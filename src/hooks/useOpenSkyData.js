3a4fe71a55197c1d0db74ed64a60bb59fb505ce8
Nils <nilsnoren99@gmail.com>
2025-06-02 00:25:15 +0200

    fel branch
    fel branch, nu fixat
    
    Co-Authored-By: Ih4z <162979441+Ih4z@users.noreply.github.com>
    Co-Authored-By: simon8056 <163146435+simon8056@users.noreply.github.com>

--- src/hooks/useOpenSkyData.js
+++ src/hooks/useOpenSkyData.js
@@ -1,48 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useOpenSkyData() {
+  // sparar data för flygplanen
   const [planes, setPlanes] = useState([]);
 
   useEffect(() => {
-    let intervalId;
-
+    // Denna funktion är för att hämta data från opensky
     async function getPlanes() {
       try {
-        const response = await fetch('https://opensky-network.org/api/states/all?lamin=49.9&lamax=59.5&lomin=-8.6&lomax=2.1');
-        const result = await response.json();
+      const response = await fetch('https://opensky-network.org/api/states/all?lamin=54.3&lamax=72&lomin=5&lomax=32');
+      const result = await response.json();
+        
+        // Hämtar den information som vi vill ha från varje plan
+        const planeData = result.states.map(index => ({
+        id: index[0],
+        callsign: index[1],
+        originCountry: index[2],
+        lat: index[6],
+        lon: index[5],
+        velocity: index[9],
+        altitude: index[7],
+      }));
 
-        if (!result.states) {
-          setPlanes([]); // No data fallback
-          return;
-        }
-
-        const planeData = result.states.map(plane => ({
-          id: plane[0],
-          callsign: plane[1].trim(),
-          originCountry: plane[2],
-          lat: plane[6],
-          lon: plane[5],
-          velocity: plane[9],
-          altitude: plane[7],
-        }));
+        console.log('bueno data (GOOOD)', planeData);
 
         setPlanes(planeData);
-        console.log("Fetched from API");
       } catch (error) {
-        console.error("Error fetching plane data", error);
+        console.log('Data finito (BAAAAD)', error);
       }
     }
 
-    // Initial fetch
     getPlanes();
-
-    // Set interval to fetch every 10 seconds (10000 ms)
-    intervalId = setInterval(() => {
-      getPlanes();
-    }, 10000);
-
-    // Cleanup on unmount
-    return () => clearInterval(intervalId);
   }, []);
 
   return planes;
