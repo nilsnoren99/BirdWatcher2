956016733aa0b9e3ec253d110b4dd17cb64ee21d
simon8056 <simon.sjoholm01@gmail.com>
2025-06-01 20:38:21 +0200

    Fixat listan för plan
    Fixat så att plan inom användarens område läggs till i listan i komponenten "PlaneList.jsx"
    
    Co-Authored-By: Ih4z <162979441+Ih4z@users.noreply.github.com>
    Co-Authored-By: Nils Norén <115877302+nilsnoren99@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './components/Navbar';
 import MapView from './components/MapView/MapView';
 import Footer from './components/Footer';
@@ -9,14 +9,15 @@ import PlaneList from './components/PlaneList';
 import FRTest from './components/FRTest';
 
 export default function App() {
+    const [planesNearby, setPlanesNearby] = useState([]);
     return (
         <div>
             <Navbar />
             <Hero />
             <div className="mapview-overlay-container">
-                <MapView />
+                <MapView setPlanesNearby={setPlanesNearby} />
                 <div className="planelist-overlay">
-                    <PlaneList />
+                    <PlaneList planes={planesNearby} />
                 </div>
             </div>
 
