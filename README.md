a0420104c4180af94d56c7bf52f3406ba5b3cc91
Nils <nilsnoren99@gmail.com>
2025-06-04 23:25:35 +0200

    Uppdaterat AboutContent.jsx & README.md
    Uppdaterat AboutContent.jsx & README.md
    
    Co-Authored-By: Ih4z <162979441+Ih4z@users.noreply.github.com>
    Co-Authored-By: simon8056 <163146435+simon8056@users.noreply.github.com>

--- README.md
+++ README.md
@@ -8,27 +8,33 @@ BirdWatcher2 is a live flight tracking web application that displays airplanes o
 
 ## Features
 
----
+
 
 - Live airplane data that updates automatically every 10 seconds without refreshing
 - Interactive map using Leaflet
 
 
 
-
+---
 
 ## Tech
----
+
 
 - **Frontend**: React, JavaScript
 - **Maps**: Leaflet
 - **Styling**: Bootstrap, CSS
 - **API**: OpenSky Network API
 
+---
 
+### Prerequisites
+Before running this project, make sure you have:
+- [**Node.js**](https://nodejs.org/)
+
+---
 
 ## Installation
----
+
 
 1. Clone the GitHub repository
 2. Open the project folder in VSCode
@@ -38,5 +44,29 @@ BirdWatcher2 is a live flight tracking web application that displays airplanes o
 6. Open the link in the terminal or manually go to: `http://localhost:5173` on your browser
 
 ---
+
+## Authors
+- **Nils Norén** - [@nilsnoren99](https://github.com/nilsnoren99)
+- **Simon Sjöholm** - [@simon8056](https://github.com/simon8056)
+- **Wilmer Nordblad Lundin** - [@Ih4z](https://github.com/Ih4z)
+
+---
+
+
+
+## Varför React? (Och inte Vue eller Angular, t.ex?)
+
+
+Vi valde React ganska tidigt i projektet, främst för att det är ett ramverk som vi alla tre redan har erfarenhet av. Det gjorde att vi snabbt kunde komma igång och fokusera på att börja jobba direkt snarare än att först behöva lära oss ett nytt verktyg.
+
+Eftersom vi är bekväma med Reacts struktur, kändes det naturligt att bygga gränssnittet med de komponenter vi behövde. Vi uppskattar även användningen av JSX, där vi kan skriva både HTML & JavaScript-kod i samma fil. För oss gör det koden mer smidig att arbeta med. Vue erbjuder en liknande struktur, men Reacts sätt att tänka passar oss bättre rent praktiskt. En annan stor fördel är React Hooks, där vi enkelt kunde samla vår logik i separata och återanvändbara funktioner, t.ex vid hantering av flygdatan eller API-anrop.
+
+React har också en stor och aktiv community. Detta gör det enkelt att hitta hjälp och lösningar i dokumentation, olika forum & YouTube-videos. Det är dessutom ett av de mest använda frontend-ramverken i arbetslivet idag, vilket gör det extra relevant att ha erfarenhet av.
+
+Vi är medvetna om att Vue hade kunnat fungera minst lika bra för ett mindre projekt som vårt, särskilt eftersom Vue också är nybörjarvänligt. I efterhand hade det kanske varit intressant att testa det. Varför vi uteslöt Angular direkt från början var genom en snabb googling då ingen av oss hade tidigare arbetat med det. Vi fick då snabbt reda på att Angular till mestadels är till för lite större och mer skalbara projekt. Enligt vår uppfattning hade det varit ett bättre alternativ för “företagsprojekt”, alltså inte passande för oss. Utöver detta använder Angular TypeScript som standard, och detta gjorde det ännu mer uppenbart att det inte var något som passade för vårt projekt.
+
+I slutändan blev valet React tack vare våra tidigare erfarenheter, bra tillgång till resurser och dess relevans på arbetsmarknaden.
+
+--- 
 
 ![Birdy](src/assets/birdy.png)
--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logoImg from '../../assets/logo.png';
 
-import './AboutContent.css'; // kanske ta bort denna importen om den inte behövs
+import './AboutContent.css';
 
 export default function AboutContent() {
     return (
@@ -11,25 +11,34 @@ export default function AboutContent() {
                     <div className="mx-auto"></div>
                     <h1 className="text-center mb-4 fort-title">Om BirdWatcher2</h1>
                     <h2 className="fort-title">Bakgrund</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, incidunt magnam numquam laudantium eveniet optio voluptatum dolores suscipit beatae aliquid consectetur assumenda nihil sed sequi, doloremque, maiores ducimus eum similique!</p>
-                    <h2 className="fort-title">Ramverk och Bibliotek med länkar</h2>
+                    <p>Så när man inte har något bättre för sig händer det att man ibland hamnar och kollar runt på Flightradar. Man kollar ofta runt bland de mest sedda planen men tillslut hamnar man oftast runt där man befinner sig för att se vilka flygplan som passerar ovanför en. Tanken med vårt projekt var att vi skulle använda oss av just flightradars API i kombination med andra ramverk och bibliotek för att lyckas skapa en webbapplikation som känner av när ett flygplan kommer innanför en distans, där man som människa kan se det. Tanken var att det skulle vara lite som ett modernt sätt till att fågelskåda, och där av namnet "BirdWatcher2" som vi nu döpt projektet till. 2an står ju för att det är en annan typ av fågelskådning, där man kan se vanlig fågelskådning som 1.</p>
+
+                    <p>Så det med hjälp av ett webb-API får vi information om plan i världen. Vi har dock valt att avgränsa planen till Norden eftersom vi ändå inte kommer kunna se plan som befinner sig i andra delar av världen. Vi har använt oss av Leaflet för att få fram en karta, geolocation för att få fram användarens plats och sedan har vi skapat en cirkel runt användarens plats som sträcker sig 20km.</p>
+
+                    <p>När ett plan kommer innanför cirkeln så sparas den automatiskt till en lista som håller koll på vilka plan som har kommit förbi.</p>
+
+                    <h2 className="fort-title">Ramverk & Bibliotek med länkar</h2>
                     <ul>
                         <li><a href="https://reactjs.org/" target="_blank">React</a></li>
                         <li><a href="https://leafletjs.com/" target="_blank">Leaflet</a></li>
+                        <li><a href="https://getbootstrap.com/" target="_blank">Bootstrap</a></li>
                         <li><a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a></li>
-                        <li><a href="https://opensky-network.org/apidoc/index.html" target="_blank">OpenSky API</a></li>
+                       
                     </ul>
-                    <h2 className="fort-title">APIer</h2>
+                    <h2 className="fort-title">API</h2>
                     <p><a href="https://opensky-network.org/apidoc/index.html" target="_blank">OpenSky API</a></p>
-                    <h2 className="fort-title">Varför vi har valt dessa?</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quas debitis ipsum harum natus enim repudiandae accusamus corporis, iusto dicta maiores illo hic illum dolorem! Iure magnam quia magni at!</p>
-                    <h2 className="fort-title">Reflektion</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, ipsum reprehenderit? Odit autem voluptatem officiis blanditiis dolorem, veniam rerum ipsam neque, animi maiores perspiciatis, obcaecati ex saepe alias illo in!</p>
+                    <h2 className="fort-title">Varför React? (& inte Vue eller Angular, t.ex?)</h2>
+                    <p>Vi valde React ganska tidigt i projektet, främst för att det är ett ramverk som vi alla tre redan har erfarenhet av. Det gjorde att vi snabbt kunde komma igång och fokusera på att börja jobba direkt snarare än att först behöva lära oss ett nytt verktyg.</p>
+                    <p>Eftersom vi är bekväma med Reacts struktur, kändes det naturligt att bygga gränssnittet med de komponenter vi behövde. Vi uppskattar även användningen av JSX, där vi kan skriva både HTML & JavaScript-kod i samma fil. För oss gör det koden mer smidig att arbeta med. Vue erbjuder en liknande struktur, men Reacts sätt att tänka passar oss bättre rent praktiskt. En annan stor fördel är React Hooks, där vi enkelt kunde samla vår logik i separata och återanvändbara funktioner, t.ex vid hantering av flygdatan eller API-anrop.</p>
+                    <p>React har också en stor och aktiv community. Detta gör det enkelt att hitta hjälp och lösningar i dokumentation, olika forum & YouTube-videos. Det är dessutom ett av de mest använda frontend-ramverken i arbetslivet idag, vilket gör det extra relevant att ha erfarenhet av.</p>
+                    <p>Vi är medvetna om att Vue hade kunnat fungera minst lika bra för ett mindre projekt som vårt, särskilt eftersom Vue också är nybörjarvänligt. I efterhand hade det kanske varit intressant att testa det. Varför vi uteslöt Angular direkt från början var genom en snabb googling då ingen av oss hade tidigare arbetat med det. Vi fick då snabbt reda på att Angular till mestadels är till för lite större och mer skalbara projekt. Enligt vår uppfattning hade det varit ett bättre alternativ för “företagsprojekt”, alltså inte passande för oss. Utöver detta använder Angular TypeScript som standard, och detta gjorde det ännu mer uppenbart att det inte var något som passade för vårt projekt.</p>
+                    <p>I slutändan blev valet React tack vare våra tidigare erfarenheter, bra tillgång till resurser och dess relevans på arbetsmarknaden.</p>
+                    
                     <h2 className="fort-title">Vi som har utvecklat sidan:</h2>
                     <ul>
-                        <li>Pils Norén</li>
-                        <li>Samuel Sjöholm</li>
-                        <li>Grand Marshal Wilmer Nordblad Lundin</li>
+                        <li>Nils Norén</li>
+                        <li>Simnon Sjöholm</li>
+                        <li>Wilmer Nordblad Lundin</li>
                     </ul>
                     <h2 className="fort-title">GitHub</h2>
                     <p>Du kan hitta koden för BirdWatcher2 på vår <a href="https://github.com/nilsnoren99/BirdWatcher2" target="_blank">GitHub-sida</a>.</p>
